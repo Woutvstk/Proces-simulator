fd680b08c436588278a319d5c9c7ddeeb9859179
Ethosssss <kobe.decapmaker@hotmail.com>
2025-10-06 11:10:53 +0200

    Update GUI.py

--- src/User_Interface/GUI.py
+++ src/User_Interface/GUI.py
@@ -3,6 +3,26 @@ import tkinter as tk
 # Maak een nieuw venster
 root = tk.Tk()
 root.title("PID Regelaar Tank")
-root.geometry("400x300")  
+
+# Start het venster fullscreen in plaats van een vaste grootte in te stellen.
+# F11 togglet fullscreen, Escape verlaat fullscreen.
+is_fullscreen = True
+root.attributes('-fullscreen', True)
+
+def toggle_fullscreen(event=None):
+	"""Toggle fullscreen (gebonden aan F11)."""
+	global is_fullscreen
+	is_fullscreen = not is_fullscreen
+	root.attributes('-fullscreen', is_fullscreen)
+
+def exit_fullscreen(event=None):
+	"""Verlaat fullscreen (gebonden aan Escape)."""
+	global is_fullscreen
+	is_fullscreen = False
+	root.attributes('-fullscreen', False)
+
+# Bind toetsen
+root.bind('<F11>', toggle_fullscreen)
+root.bind('<Escape>', exit_fullscreen)
 
 root.mainloop()
